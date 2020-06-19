1ce24ab02e5cce56158cf7f496b187e887cc385a
Hitesh Jain <hitesh.jain@usabilla.com>
2020-06-19 17:17:04 +0200

    Update Sample App

--- example/lib/usabilla.dart
+++ example/lib/usabilla.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_usabilla/flutter_usabilla.dart';
 
@@ -11,12 +12,14 @@ class UsabillaDemoApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        buttonColor: Color(0xff00a5c9),
+        buttonTheme: ButtonThemeData(
+          //  <-- dark color
+          textTheme:
+              ButtonTextTheme.primary, //  <-- this auto selects the right color
+        ),
       ),
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Usabilla Demo'),
-        ),
         body: Center(
           child: HomeWidget(),
         ),
@@ -32,6 +35,20 @@ class HomeWidget extends StatefulWidget {
   _HomeWidgetState createState() => _HomeWidgetState();
 }
 
+Widget createButton(btnText, btnMethod) {
+  return RaisedButton(
+    child: Text(
+      btnText,
+      style: TextStyle(fontSize: 15),
+    ),
+    padding: const EdgeInsets.all(15.0),
+    textColor: Colors.white,
+    color: Color(0xff00a5c9),
+    onPressed: btnMethod,
+    textTheme: ButtonTextTheme.normal,
+  );
+}
+
 class _HomeWidgetState extends State<HomeWidget> {
   String os = Platform.operatingSystem;
   String _platformVersion = 'Unknown';
@@ -40,24 +57,29 @@ class _HomeWidgetState extends State<HomeWidget> {
   String _event = 'YOUR_EVENT_TAG_HERE';
   String _defaultMaskCharacter = 'YOUR_DEFAULT_MASK_CHARACTER_HERE';
   List _defaultDataMask = [];
+  List<String> _formIds = [];
   Map customVariable = {'YOUR_KEY_HERE': 'YOUR_VALUE_HERE'};
+  String _localizedStringFilename = 'YOUR_LOCALE_FILE_NAME_HERE';
+
+  final textFieldController = TextEditingController();
+  bool _validate = false;
+
+  @override
+  void dispose() {
+    textFieldController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
     initialize();
-    setCustomVariable();
-    getDefaultData();
-  }
-
-  Future<void> setCustomVariable() async {
-    print('custom variable started');
-    try {
-      await FlutterUsabilla.setCustomVariables(customVariable);
-    } on PlatformException {
-      print('Failed to get platform version.');
-    }
+//    setDebugEnabled();
+//    setCustomVariable();
+//    getDefaultData();
+//    localizedStringFile();
+//    _formIds.add(_formId);
   }
 
   Future<void> initPlatformState() async {
@@ -85,6 +107,37 @@ class _HomeWidgetState extends State<HomeWidget> {
     }
   }
 
+  Future<void> sendEvent() async {
+    print('Send Event clicked');
+    Map ubResult;
+    String event = textFieldController.text;
+    if (event.isNotEmpty) {
+      setState(() {
+        _validate = false;
+      });
+      try {
+        ubResult = await FlutterUsabilla.sendEvent(event);
+      } on PlatformException {
+        print('Failed to sendEvent.');
+      }
+      print('result - $ubResult');
+    } else {
+      print('Value Can\'t Be Empty');
+      setState(() {
+        _validate = true;
+      });
+    }
+  }
+
+  Future<void> resetEvents() async {
+    print('reset event clicked');
+    try {
+      await FlutterUsabilla.resetCampaignData();
+    } on PlatformException {
+      print('Failed to resetCampaignData.');
+    }
+  }
+
   Future<void> showForm() async {
     print('show Form clicked');
     Map ubResult;
@@ -94,6 +147,34 @@ class _HomeWidgetState extends State<HomeWidget> {
       print('Failed to loadFeedbackForm.');
     }
     print('result - $ubResult');
+  }
+
+/*
+  Future<void> setDebugEnabled() async {
+    print('setDebugEnabled started');
+    try {
+      await FlutterUsabilla.setDebugEnabled(false);
+    } on PlatformException {
+      print('Failed to setDebugEnabled.');
+    }
+  }
+
+  Future<void> localizedStringFile() async {
+    print('localizedStringFile started');
+    try {
+      await FlutterUsabilla.localizedStringFile(_localizedStringFilename);
+    } on PlatformException {
+      print('Failed to localizedStringFile.');
+    }
+  }
+
+  Future<void> setCustomVariable() async {
+    print('custom variable started');
+    try {
+      await FlutterUsabilla.setCustomVariables(customVariable);
+    } on PlatformException {
+      print('Failed to get platform version.');
+    }
   }
 
   Future<void> showFormWithScreenshot() async {
@@ -107,26 +188,6 @@ class _HomeWidgetState extends State<HomeWidget> {
       print('Failed to loadFeedbackFormWithCurrentViewScreenshot.');
     }
     print('result - $ubResult');
-  }
-
-  Future<void> sendEvent() async {
-    print('send event clicked');
-    Map ubResult;
-    try {
-      ubResult = await FlutterUsabilla.sendEvent(_event);
-    } on PlatformException {
-      print('Failed to sendEvent.');
-    }
-    print('result - $ubResult');
-  }
-
-  Future<void> resetEvents() async {
-    print('reset event clicked');
-    try {
-      await FlutterUsabilla.resetCampaignData();
-    } on PlatformException {
-      print('Failed to resetCampaignData.');
-    }
   }
 
   Future<void> dismiss() async {
@@ -160,68 +221,86 @@ class _HomeWidgetState extends State<HomeWidget> {
     });
   }
 
+  Future<void> preloadForms() async {
+    print('preloadForms clicked $_formId');
+    !_formIds.contains(_formId) ?? _formIds.add(_formId);
+    try {
+      print('preloadForms $_formIds');
+      await FlutterUsabilla.preloadFeedbackForms(_formIds);
+    } on PlatformException {
+      print('Failed to preloadForms.');
+    }
+  }
+
+  Future<void> removeCachedForms() async {
+    print('removeCachedForms clicked');
+    try {
+      await FlutterUsabilla.removeCachedForms();
+    } on PlatformException {
+      print('Failed to removeCachedForms.');
+    }
+  }
+*/
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        InfoTitle(
-          title: "$os - v$_platformVersion",
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset(
+              'images/logo.png',
+              width: 200,
+            ),
+            Icon(
+              Icons.add,
+              color: Color(0xff00a5c9),
+              size: 50.0,
+            ),
+            FlutterLogo(
+              size: 75,
+            ),
+          ],
         ),
-        InfoTitle(
-          title: _defaultDataMask.toString(),
+        Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              createButton('Show Form', showForm),
+              Container(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.50,
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: textFieldController,
+                      style: TextStyle(height: 0.5, color: Colors.black),
+                      decoration: InputDecoration(
+                          errorText: _validate ? 'Value Can\'t Be Empty' : null,
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter Event here'),
+                    ),
+                  ),
+                  createButton('Send Event', sendEvent),
+                ],
+              ),
+              Container(height: 20),
+              createButton('Reset', resetEvents),
+            ],
+          ),
         ),
-        RaisedButton(
-          child: Text('Show Form'),
-          onPressed: showForm,
-          textTheme: ButtonTextTheme.accent,
-        ),
-        RaisedButton(
-          child: Text('Show Form With Screenshot'),
-          onPressed: showFormWithScreenshot,
-          textTheme: ButtonTextTheme.accent,
-        ),
-        RaisedButton(
-          child: Text('Dismiss'),
-          onPressed: dismiss,
-          textTheme: ButtonTextTheme.accent,
-        ),
-        RaisedButton(
-          child: Text('Send Event'),
-          onPressed: sendEvent,
-          textTheme: ButtonTextTheme.accent,
-        ),
-        RaisedButton(
-          child: Text('Reset'),
-          onPressed: resetEvents,
-          textTheme: ButtonTextTheme.accent,
-        ),
-        RaisedButton(
-          child: Text('setDataMasking'),
-          onPressed: setDataMasking,
-          textTheme: ButtonTextTheme.accent,
+        Image.asset(
+          'images/footer.png',
+          width: 100,
         ),
       ],
-    );
-  }
-}
-
-class InfoTitle extends StatelessWidget {
-  InfoTitle({this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(
-        child: Text(
-          'Running on: $title\n',
-          textAlign: TextAlign.center,
-        ),
-      ),
     );
   }
 }
