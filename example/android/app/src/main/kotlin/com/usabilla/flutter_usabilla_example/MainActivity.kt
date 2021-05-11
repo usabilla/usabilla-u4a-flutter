d8790449a7e4d4d519b48675a6e55a29164ab6ec
Lorenzo Marchiori <lmarchiori@surveymonkey.com>
2021-05-11 13:53:01 +0200

    Release v1.1.0

--- example/android/app/src/main/kotlin/com/usabilla/flutter_usabilla_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/usabilla/flutter_usabilla_example/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.usabilla.flutter_usabilla_example
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
