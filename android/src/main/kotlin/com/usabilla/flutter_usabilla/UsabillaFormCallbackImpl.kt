f6589ba5e219c31530af2c69a7fa47070fea160e
Lorenzo Marchiori <lmarchiori@surveymonkey.com>
2021-05-11 13:52:01 +0200

    fix(nullable): UsabillaFormCallback applies null safety

--- android/src/main/kotlin/com/usabilla/flutter_usabilla/UsabillaFormCallbackImpl.kt
+++ android/src/main/kotlin/com/usabilla/flutter_usabilla/UsabillaFormCallbackImpl.kt
@@ -1,8 +1,6 @@
 package com.usabilla.flutter_usabilla
 
-import android.R
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import com.usabilla.flutter_usabilla.FlutterUsabillaPlugin.Companion.FRAGMENT_TAG
 import com.usabilla.flutter_usabilla.FlutterUsabillaPlugin.Companion.KEY_ERROR_MSG
 import com.usabilla.flutter_usabilla.FlutterUsabillaPlugin.Companion.activity
@@ -12,25 +10,25 @@ import com.usabilla.sdk.ubform.sdk.form.FormClient
 
 class UsabillaFormCallbackImpl : UsabillaFormCallback {
 
-    override fun formLoadSuccess(formClient: FormClient) {
-        formClient?.let {
-            val form = formClient.fragment
-            val supportFragmentManager: FragmentManager = (activity as FragmentActivity).supportFragmentManager
-            supportFragmentManager.beginTransaction().replace(R.id.content, form, FRAGMENT_TAG).commit()
-        }
+    override fun formLoadSuccess(form: FormClient) {
+        val formFragment = form.fragment
+        (activity as FragmentActivity).supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, formFragment, FRAGMENT_TAG)
+            .commit()
     }
 
-    override fun mainButtonTextUpdated(s: String) {
-        val mainButtonTextArg: String = "mainButtonText"
-        var result: Map<String, Any> = mapOf<String, Any>(mainButtonTextArg to s!!)
-        //FIXME Need to fix mainButtonTextUpdated
-        // - when it will be implemented.
-        //response(context, "MainButtonTextUpdated", result)
+    override fun mainButtonTextUpdated(text: String) {
+        // FIXME Need to fix mainButtonTextUpdated
+        // val mainButtonTextArg = "mainButtonText"
+        // var result: Map<String, Any> = mapOf<String, Any>(mainButtonTextArg to text)
+        // response(context, "MainButtonTextUpdated", result)
     }
 
     override fun formLoadFail() {
-        val res: Map<String, Any> = mapOf<String, Any>(KEY_ERROR_MSG to "The form could not be loaded")
-        ubFormResult?.success(res)
+        val res: Map<String, Any> =
+            mapOf<String, Any>(KEY_ERROR_MSG to "The form could not be loaded")
+        ubFormResult.success(res)
         ubFormResult = null
     }
 }
